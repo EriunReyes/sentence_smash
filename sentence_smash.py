00672755d4d5c2fa9eac38445cfc82e50acd2f7f
EriunReyes <eriun1227@gmail.com>
2023-04-20 20:08:32 -0400

    updated file

--- sentence_smash.py
+++ sentence_smash.py
@@ -1,4 +1,4 @@
 def smash(word: list) -> str:
     return ''.join([''.join(i + " ") if i != word[-1] and len(word) != 1 \
-                    else ''.join(i.replace(",", " ")) for i in word])
+                    else ''.join(i) for i in word])
     
